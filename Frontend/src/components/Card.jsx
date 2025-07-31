8b795589c1bdb56743159919d9de82176c7b06bf
SergioSuarezG4 <chechonieto235@gmail.com>
2025-07-30 21:58:56 -0500

    Se modifico el componente Card Envolviendolo en un NavLink para navegar entre las rutas segun su tipo de Card

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,12 +1,17 @@
+import { NavLink } from "react-router";
 const Card = ({title, description, icon:Icon }) => {
     return(
-        <div className="bg-white p-8 rounded-lg border border-[#d9d9d9] hover:border-[#4da6ff] transition-all duration-200 hover:shadow-lg cursor-pointer">
+    <NavLink
+    to={`/${title}`}>
+
+        <div className="bg-white max-w-[254px] p-8 rounded-lg border border-[#d9d9d9] hover:border-[#4da6ff] transition-all duration-200 hover:shadow-lg cursor-pointer">
             <div className="mx-auto h-16 w-16 bg-[#4da6ff]/10 rounded-full flex items-center justify-center mb-4">
                 <Icon className="text-[#4da6ff] text-3xl"/>
             </div>
             <h3 className="text-gray-900 text-xl font-semibold mb-2">{title}</h3>
             <p className="text-gray-600">{description}</p>
         </div>
+    </NavLink>
     )    
 }
 
